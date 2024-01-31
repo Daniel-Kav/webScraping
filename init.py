56699e331b0079422e91867341f30a7f9a0853c3
daniel-kav <dankavatha01@gmail.com>
2024-01-31 18:04:30 +0300

    requests

--- init.py
+++ init.py
@@ -1,5 +1,10 @@
 import requests
+from bs4  import BeautifulSoup
+
 url = 'https://understat.com/team/Arsenal/2023'
 r = requests.get(url)
-print(r.status_code, r.headers)
+print(r)
 #print(r.text)
+
+soup = BeautifulSoup(r.text,"lxml")
+print(soup)
